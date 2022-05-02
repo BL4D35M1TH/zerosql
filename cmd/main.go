54fa254df3879ae734ee983822915b4c74ad46c6
aaronsmith <aaronthebladesmith@gmail.com>
2022-05-02 10:18:45 +0530

    add config support

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"sanndy/server"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type Config struct {
+	Directory string `json:"directory"`
+	Database  string `json:"database"`
+	Address   string `json:"address"`
+}
+
 func main() {
-	imageServer, err := server.CreateServer("./root", "./test.db")
+	var config Config
+	file, err := os.ReadFile("./config.json")
+	if err != nil {
+		panic(fmt.Errorf("unable to read file: %w", err))
+	}
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		panic(fmt.Errorf("unable to parse config: %w", err))
+	}
+	imageServer, err := server.CreateServer(config.Directory, config.Database)
 	if err != nil {
 		panic(err)
 	}
-	panic(http.ListenAndServe(":8080", imageServer))
+	panic(http.ListenAndServe(config.Address, imageServer))
 }
